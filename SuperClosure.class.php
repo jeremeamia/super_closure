80e060d1bf9609c1f60b9c6bf4b74f5700410746
unknown <Synapse@.(none)>
2010-01-20 16:40:51 -0700

    Added some comments to explain complex parts of code

--- SuperClosure.class.php
+++ SuperClosure.class.php
@@ -18,9 +18,11 @@ class SuperClosure {
 
 	public function __construct($function)
 	{
+		// Check if parameter is an actual closure
 		if ( ! $function instanceOf Closure)
 			throw new InvalidArgumentException();
 
+		// Set the member variable values
 		$this->closure = $function;
 		$this->reflection = new ReflectionFunction($function);
 		$this->code = $this->_fetchCode();
@@ -43,15 +45,19 @@ class SuperClosure {
 
 	protected function _fetchCode()
 	{
-		$code = '';
+		// Open file and seek to the first line of the closure
 		$file = new SplFileObject($this->reflection->getFileName());
 		$file->seek($this->reflection->getStartLine()-1);
+
+		// Retrieve all of the lines that contain code for the closure
+		$code = '';
 		while ($file->key() < $this->reflection->getEndLine())
 		{
 			$code .= $file->current();
 			$file->next();
 		}
 
+		// Only keep the code defining that closure
 		$begin = strpos($code, 'function');
 		$end = strrpos($code, '}');
 		$code = substr($code, $begin, $end - $begin + 1);
@@ -74,16 +80,20 @@ class SuperClosure {
 
 	protected function _fetchUsedVariables()
 	{
+		// Make sure the use construct is actually used
 		$use_index = stripos($this->code, 'use');
 		if ( ! $use_index)
 			return array();
 
+		// Get the names of the variables inside the use statement
 		$begin = strpos($this->code, '(', $use_index) + 1;
 		$end = strpos($this->code, ')', $begin);
-
-		$static_vars = $this->reflection->getStaticVariables();
 		$vars = explode(',', substr($this->code, $begin, $end - $begin));
 
+		// Get the static variables of the function via reflection
+		$static_vars = $this->reflection->getStaticVariables();
+		
+		// Only keep the variables that appeared in both sets
 		$used_vars = array();
 		foreach ($vars as $var)
 		{
@@ -103,15 +113,20 @@ class SuperClosure {
 
 	public function __sleep()
 	{
+		// Only serialize the code and used_variables, the closure or reflection members with cause a fatal error
 		return array('code', 'used_variables');
 	}
 
 
 	public function __wakeup()
 	{
+		// Import the used variables so they can again be inherited by the closure
 		extract($this->used_variables);
 
+		// Eval the closure's code to recreate the closure
 		eval('$_function = '.$this->code.';');
+
+		// If the eval succeeded create the reflection object as well
 		if (isset($_function) AND $_function instanceOf Closure)
 		{
 			$this->closure = $_function;

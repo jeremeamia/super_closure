574829674520ebea5953e0adeb05abbda55faa29
Graham Campbell <graham@mineuk.com>
2014-09-22 20:15:59 +0100

    Moved test autoloading to composer.json autoload-dev

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,3 @@ date_default_timezone_set('UTC');
 
 // Register and fetch the Composer autoloader
 $loader = require __DIR__ . '/../vendor/autoload.php';
-
-// Add the Test namespace to the autoloader
-$loader->addPsr4('SuperClosure\\Test\\Unit\\', __DIR__ . '/Unit');
-$loader->addPsr4('SuperClosure\\Test\\Integration\\', __DIR__ . '/Integration');

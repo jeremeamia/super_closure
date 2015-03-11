fec88d7d4e311f321a412102df14d8696257f022
Jeremy Lindblom <jeremeamia@gmail.com>
2015-03-11 12:04:34 -0700

    Fixes #44 - Returns $this->data in __debugInfo, if available, to avoid a call to an undefined $serializer, in the case of an unserialized closure.

--- src/SerializableClosure.php
+++ src/SerializableClosure.php
@@ -161,6 +161,6 @@ class SerializableClosure implements \Serializable
      */
     public function __debugInfo()
     {
-        return $this->serializer->getData($this->closure);
+        return $this->data ?: $this->serializer->getData($this->closure, true);
     }
 }
